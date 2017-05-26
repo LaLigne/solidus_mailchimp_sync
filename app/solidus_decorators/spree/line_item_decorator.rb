bd24d7849cc760b1d12fc84722e65ebea21e79ac
Andrew Lim <andrew.lim@nolimitzone.com>
2017-05-26 10:47:21 +1000

    Reduce errors for product/order deletes

--- app/solidus_decorators/spree/line_item_decorator.rb
+++ app/solidus_decorators/spree/line_item_decorator.rb
@@ -2,7 +2,9 @@ Spree::LineItem.class_eval do
   after_commit :mailchimp_sync
 
   def mailchimp_sync
-    # If a LineItem changes, tell the order to Sync for sure.
-    SolidusMailchimpSync::OrderSynchronizer.new(order).auto_sync(force: true)
+    unless order.deleted?
+      # If a LineItem changes, tell the order to Sync for sure.
+      SolidusMailchimpSync::OrderSynchronizer.new(order).auto_sync(force: true)
+    end
   end
 end
